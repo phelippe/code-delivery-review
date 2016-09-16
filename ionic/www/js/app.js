00824fbae5071b28e8b33ce6d60d8b499adbb3ac
Phelippe Matte <phelippematte@mobister.com.br>
2016-09-16 13:51:01 -0300

    Trabalhando com navegação e titulo

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,18 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+    .config(function( $stateProvider, $urlRouterProvider ){
+
+      $stateProvider
+          .state('home', {
+            url: '/home/:nome',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeCtrl',
+          })
+          .state('main', {
+            url: '/',
+            templateUrl: 'templates/main.html',
+          });
+      $urlRouterProvider.otherwise('/');
+    });
